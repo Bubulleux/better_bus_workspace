1a86b09c891b4f465210ba4b7b18abc90142a1a5
Ulysse <bubulle86@yahoo.fr>
2025-06-30 20:28:18 +0200

    Nice Real time

--- better_bus_angouleme/lib/main.dart
+++ better_bus_angouleme/lib/main.dart
@@ -1,10 +1,10 @@
-import 'mobius_downloader.dart';
 import 'package:better_bus_v2/better_bus.dart';
 import 'package:better_bus_v2/data_provider/app_provider.dart';
 import 'package:better_bus_v2/model/app_config.dart';
 import 'package:better_bus_v2/model/app_paths.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter/material.dart';
+import 'package:better_bus_core/core.dart';
 
 class MobiusConfig extends AppConfig {
   MobiusConfig()
